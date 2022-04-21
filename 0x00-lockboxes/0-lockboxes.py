b001dbfa11bd50cd219d7cc6879cbe507811c609
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-04-21 00:36:11 -0500

    Lockboxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -8,12 +8,10 @@ def canUnlockAll(boxes):
     """ solve the problem of
     loockboxes """
     unlocked = [0]
-    for box_id, box in enumerate(boxes):
-        if not box:
-            continue
-    for key in box:
-        if key < len(boxes) and key not in unlocked and key != box_id:
-            unlocked.append(key)
+    for box_id, box in enumerate(boxes, 1):
+        for key in box:
+            if key < len(boxes) and key not in unlocked and key != box_id:
+                unlocked.append(key)
     if len(unlocked) == len(boxes):
         return True
     return False
