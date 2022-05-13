dbd642efcd281ee62474c95ba57bec0d1973c689
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-05-12 19:10:11 -0500

    Minimum Operations

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,14 @@
+"""
+0x03. Minimum Operations"""
+
+
+def minOperations(n):
+    """
+    Calculates the fewest number of operations
+    needed to result in exactly n H characters in the file."""
+    totalOperations = 0
+    for i in range(2, n + 1):
+        while n % i == 0:
+            totalOperations += i
+            n = n / i
+    return totalOperations
