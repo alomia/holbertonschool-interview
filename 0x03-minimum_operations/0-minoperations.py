18f8f1bbbe6e1e6a1ee6bae8e2e2981d072fac05
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-05-12 19:34:26 -0500

    Minimum Operations

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 """
-0x03. Minimum Operations
+Minimum Operations
 """
 
 
 def minOperations(n):
     """
     Calculates the fewest number of operations
-    needed to result in exactly n H characters in the file."""
+    needed to result in exactly n H characters in the file.
+    """
+
+    if n < 2 or type(n) is not int:
+        return 0
+
     totalOperations = 0
 
-    if n > 1:
-        for i in range(2, n + 1):
-            while n % i == 0:
-                totalOperations += i
-                n = n / i
+    for i in range(2, n + 1):
+        while n % i == 0:
+            totalOperations += i
+            n = n / i
 
     return totalOperations
